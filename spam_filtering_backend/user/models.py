941a62fd31f3f8e9ceb5618fbd92d28633369511
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-11-30 09:06:16 +0100

    add user model to django backend

--- spam_filtering_backend/user/models.py
+++ spam_filtering_backend/user/models.py
@@ -1,3 +1,14 @@
+"""Model classes for saving objects with certain structure into the database."""
 from django.db import models
 
+
+
 # Create your models here.
+class User(models.Model):
+    """User class to model a potential user of the spam_filtering_analysis application"""
+
+    user_id = models.IntegerField(
+        primary_key=True, blank=False, null=False, unique=True
+    )
+    username = models.CharField(max_length=70, blank=False, default="")
+    password = models.CharField(max_length=100)
