f9526d19c0fe76c0236b5495fd41d82e8611dc11
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-16 21:07:42 +0100

    fix typo - updated requirements.txt - fix api

--- spam_filtering_backend/api/urls.py
+++ spam_filtering_backend/api/urls.py
@@ -1,5 +1,9 @@
+"""Endpoints for the API app"""
 from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("auth", views.authenticate, name="authenticate"),
+    path("classify", views.classify, name="classify"),
+]
--- spam_filtering_backend/api/views.py
+++ spam_filtering_backend/api/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
+"""Views for the API app"""
+import json
 import os
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import joblib
 
 
-def index(request):
-    return JsonResponse({"key": os.environ["AUTH_TOKEN"]})
+@csrf_exempt
+def authenticate(request):
+    """Authentification view to validate a token from the frontend."""
+
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+            token = data.get("token", "")
+            valid = os.environ.get("AUTH_TOKEN", "") == token
+            return JsonResponse({"validated": valid})
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON in request"}, status=400)
+
+    return JsonResponse({"error": "Invalid request method"}, status=405)
+
+
+@csrf_exempt
+def classify(request):
+    """Classify view for classifying e-mail texts with a count vectorizer."""
+    if request.method == "POST":
+        model = joblib.load("spam_filtering_backend/data/spam_classifier_model.joblib")
+        count_vectorizer = joblib.load(
+            "spam_filtering_backend/data/count_vectorizer.joblib"
+        )
+        print(request.body)
+        data = json.loads(request.body.decode("utf-8"))
+        input_array = []
+        input_array.append(data.get("text", ""))
+        input_features = count_vectorizer.transform(input)
+        prediction = model.predict(input_features)
+        print(prediction)
+        return JsonResponse({"prediction": int(prediction[0])})
+    return JsonResponse({"error": "Invalid request method"}, status=405)
